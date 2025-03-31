9db8070c5b7bb277800f169d3d090b9d10f6f85a
Don <serfdon@gmail.com>
2025-03-31 09:44:23 -0700

    Fixed fences

--- init.lua
+++ init.lua
@@ -1,10 +1,24 @@
 
 if minetest.get_modpath("my_door_wood") then
 
-	minetest.register_node("mydoorwoodfences:fence_black", {
-		description = "Black Fence",
+	local fences = {
+					{"black","Black",},
+					{"brown","Brown",},
+					{"dark_grey","Dark Grey",},
+					{"grey","Grey",},
+					{"red","Red",},
+					{"white","White",},
+					{"yellow","Yellow",},
+					}
+					
+	for i in ipairs(fences) do
+		local col = fences[i][1]
+		local des = fences[i][2]
+--[[
+	minetest.register_node("mydoorwoodfences:fence_"..col, {
+		description = des.." Fence",
 		drawtype = "fencelike",
-		tiles = {"mydoors_black_wood.png^[transformR90"},
+		tiles = {"mydoors_"..col.."_wood.png^[transformR90"},
 		paramtype = "light",
 		is_ground_content = true,
 		selection_box = {
@@ -14,140 +28,49 @@ if minetest.get_modpath("my_door_wood") then
 		groups = {cracky = 2},
 		sounds = default.node_sound_stone_defaults(),
 	})
-	minetest.register_node("mydoorwoodfences:fence_brown", {
-		description = "Brown Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_brown_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-	minetest.register_node("mydoorwoodfences:fence_dark_grey", {
-		description = "Dark Grey Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_dark_grey_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-	minetest.register_node("mydoorwoodfences:fence_grey", {
-		description = "Grey Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_grey_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-	minetest.register_node("mydoorwoodfences:fence_red", {
-		description = "Red Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_red_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-	minetest.register_node("mydoorwoodfences:fence_white", {
-		description = "White Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_white_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-	minetest.register_node("mydoorwoodfences:fence_yellow", {
-		description = "Yellow Fence",
-		drawtype = "fencelike",
-		tiles = {"mydoors_yellow_wood.png^[transformR90"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-	})
-end
+--]]
+default.register_fence("mydoorwoodfences:fence_"..col, {
+	description = des..(" Fence"),
+	texture = "mydoors_"..col.."_wood.png^[transformR90",
+	inventory_image = "default_fence_overlay.png^mydoors_"..col.."_wood.png^[transformR90" ..
+				"default_fence_overlay.png^[makealpha:255,126,126",
+	wield_image = "default_fence_overlay.png^mydoors_"..col.."_wood.png^[transformR90" ..
+				"default_fence_overlay.png^[makealpha:255,126,126",
+	material = "mydoors_"..col.."_wood.png^[transformR90",
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	sounds = default.node_sound_wood_defaults()
+})
+--Crafts
+minetest.register_craft({
+		output = "mydoorwoodfences:fence_black 6",
+		recipe = {
+			{'','my_door_wood:wood_'..col,''},
+			{'','my_door_wood:wood_'..col,''},
+			{'','my_door_wood:wood_'..col,''}
+			}
+})
+
+--Rails
+default.register_fence_rail("mydoorwoodfences:fence_rail_"..col, {
+	description = des..(" Fence Rail"),
+	texture = "mydoors_"..col.."_wood.png^[transformR90",
+	inventory_image = "default_fence_rail_overlay.png^mydoors_"..col.."_wood.png^[transformR90.png^" ..
+				"default_fence_rail_overlay.png^[makealpha:255,126,126",
+	wield_image = "default_fence_rail_overlay.png^mydoors_"..col.."_wood.png^[transformR90.png^" ..
+				"default_fence_rail_overlay.png^[makealpha:255,126,126",
+	material = wo,
+	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	sounds = default.node_sound_wood_defaults()
+})
 
 --Crafts
 minetest.register_craft({
 		output = "mydoorwoodfences:fence_black 6",
 		recipe = {
-			{'','my_door_wood:wood_black',''},
-			{'','my_door_wood:wood_black',''},
-			{'','my_door_wood:wood_black',''}
+			{'','my_door_wood:wood_'..col,''},
+			{'','my_door_wood:wood_'..col,''},
+			{'','my_door_wood:wood_'..col,''}
 			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_brown 6",
-		recipe = {
-			{'','my_door_wood:wood_brown',''},
-			{'','my_door_wood:wood_brown',''},
-			{'','my_door_wood:wood_brown',''}
-			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_dark_grey 6",
-		recipe = {
-			{'','my_door_wood:wood_dark_grey',''},
-			{'','my_door_wood:wood_dark_grey',''},
-			{'','my_door_wood:wood_dark_grey',''}
-			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_grey 6",
-		recipe = {
-			{'','my_door_wood:wood_grey',''},
-			{'','my_door_wood:wood_grey',''},
-			{'','my_door_wood:wood_grey',''}
-			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_red 6",
-		recipe = {
-			{'','my_door_wood:wood_red',''},
-			{'','my_door_wood:wood_red',''},
-			{'','my_door_wood:wood_red',''}
-			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_white 6",
-		recipe = {
-			{'','my_door_wood:wood_white',''},
-			{'','my_door_wood:wood_white',''},
-			{'','my_door_wood:wood_white',''}
-			}
-	})
-minetest.register_craft({
-		output = "mydoorwoodfences:fence_yellow 6",
-		recipe = {
-			{'','my_door_wood:wood_yellow',''},
-			{'','my_door_wood:wood_yellow',''},
-			{'','my_door_wood:wood_yellow',''}
-			}
-	})
+})
+end
+end
